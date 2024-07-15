cb3425949a74324331217053d7dfe73cf531763d
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-15 15:25:45 -0400

    SOAP WebServices

---
+++ src/main/java/com/sam/springboot_soap_operation/springSOAPDemo/SpringBootSoapOperationApplication.java
@@ -0,0 +1,13 @@
+package com.sam.springboot_soap_operation.springSOAPDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSoapOperationApplication.class, args);
+    }
+
+}
