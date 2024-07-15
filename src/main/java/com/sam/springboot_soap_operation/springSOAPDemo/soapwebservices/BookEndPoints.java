c439d296c3a04291dce04ce556d976dbc47ac984
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-15 15:33:05 -0400

    SOAP WebServices

---
+++ src/main/java/com/sam/springboot_soap_operation/springSOAPDemo/soapwebservices/BookEndPoints.java
@@ -0,0 +1,29 @@
+package com.sam.springboot_soap_operation.springSOAPDemo.soapwebservices;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class BookEndpoints {
+    private static final String NAMESPACE_URI = "http://www.perscholas.com/xml/book";
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookEndpoints(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBookRequest")
+    @ResponsePayload
+    public GetBookResponse getCountry(@RequestPayload GetBookRequest request) {
+        GetBookResponse response = new GetBookResponse();
+        response.setBook(bookRepository.findBookById(request.getId()));
+        return response;
+    }
+}
+
